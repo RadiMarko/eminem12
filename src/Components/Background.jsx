1c6f6eee1d631f888d569e0031dfdd0d5dfe8df0
RadiMarko <r.marko1995@gmail.com>
2025-06-11 18:24:14 +0200

    Add background image

---
+++ src/Components/Background.jsx
@@ -0,0 +1,7 @@
+﻿import backGroundImage from "/background_image.jpg"
+
+export default function Background() {
+    return (
+        <img src={backGroundImage} alt="Background image of Eminem."/>
+    )
+}
